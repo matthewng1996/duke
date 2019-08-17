e24e28f40e5d9bce4f4079e4f5653a7f8fea465a
matthewng1996 <matthewngweile@hotmail.com>
2019-08-17 09:08:38 +0800

    First Commit - Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+import java.util.stream.StreamSupport;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +9,27 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        Scanner input = new Scanner(System.in);
+        String myString = new String();
+        System.out.println("______________________________");
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?\n");
+        System.out.println("______________________________");
+        myString = input.nextLine();
+
+        while (!myString.equals("bye"))
+        {
+            System.out.println("______________________________");
+            System.out.println(myString);
+            System.out.println("______________________________");
+            myString = input.nextLine();
+        }
+        if (myString.equals("bye"))
+        {
+            System.out.println("______________________________");
+            System.out.println("Bye. Hope to see you again soon!");
+            System.out.println("______________________________");
+            System.exit(0);
+        }
     }
 }
