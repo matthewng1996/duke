d843e3ddfcb2c9964fc1690b3eebdec1c8983cb2
matthewng1996 <matthewngweile@hotmail.com>
2019-08-24 20:23:22 +0800

    Level 8

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,7 @@
 import java.io.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 import java.util.ArrayList;
 
@@ -59,12 +62,14 @@ public class Duke {
                     System.out.println("______________________________");
                     System.out.println(e);
                     System.out.println("______________________________");
+                } catch (ParseException e) {
+                    e.printStackTrace();
                 }
             }
             else if (myString.contains("event")) {
                 try {
                     event(myString, taskList);
-                } catch (DukeException e) {
+                } catch (DukeException | ParseException e) {
                     System.out.println("______________________________");
                     System.out.println(e);
                     System.out.println("______________________________");
@@ -151,7 +156,7 @@ public class Duke {
         }
     }
 
-    public static void deadline (String myString, ArrayList<Task> taskList) throws DukeException{
+    public static void deadline (String myString, ArrayList<Task> taskList) throws DukeException, ParseException {
         String keyStroke = "/";
         String description = myString.substring(myString.indexOf(" ") + 1, myString.indexOf(keyStroke));
         description = description.replaceAll(" ", "");
@@ -160,17 +165,35 @@ public class Duke {
             throw new DukeException("☹ OOPS!!! The description of deadline cannot be empty.");
 
         int index = myString.indexOf(keyStroke);
+
         if (index == -1)
             throw new DukeException("Deadline comes with a /by!");
+
         else {
             String task = myString.substring(myString.indexOf(" "), index);
             String date = myString.substring(index + 3);
+            String temp = date;
+
             date = date.replaceAll(" ", "");
 
             if (date.isEmpty())
                 throw new DukeException("☹ OOPS!!! I'm sorry, but you need a date/time for your deadline");
+            else if (temp.matches(" ([0-9]{1,2})/([0-9]{1,2})/([0-9]{4}) ([0-9]{4})")) {
+                SimpleDateFormat parser = new SimpleDateFormat("dd/MM/yyy HHmm");
+                SimpleDateFormat formatter = new SimpleDateFormat("dd MMM yyy, h:mma"); //Desired format
+                Date dateInput = parser.parse(temp);
+                date = formatter.format(dateInput);
+                System.out.println("Your specified deadline is " + date);
+            } else if (temp.matches(" ([0-9]{1,2})/([0-9]{1,2})/([0-9]{4})")) {
+                SimpleDateFormat parser = new SimpleDateFormat("dd/MM/yyy");
+                SimpleDateFormat formatter = new SimpleDateFormat("dd MMM yyy"); //Desired Format
+                Date dateInput = parser.parse(temp);
+                date = formatter.format(dateInput);
+                System.out.println("Your specified deadline is " + date);
+            } else
+                System.out.println("Do note that you did not specify any deadline though.");
 
-            Task newTask = new Deadline(task, date);
+            Task newTask = new Deadline(task, temp);
             System.out.println("______________________________");
             System.out.println("Got it. I've added this task:");
             System.out.println(newTask.toString());
@@ -183,7 +206,7 @@ public class Duke {
         }
     }
 
-    public static void event (String myString, ArrayList<Task> taskList) throws DukeException{
+    public static void event (String myString, ArrayList<Task> taskList) throws DukeException, ParseException {
         String keyStroke = "/";
         int index = myString.indexOf(keyStroke);
         String description = myString.substring(myString.indexOf(" ") + 1, index);
@@ -193,17 +216,33 @@ public class Duke {
             throw new DukeException("☹ OOPS!!! The description of event cannot be empty.");
 
         if (index == -1)
-            throw new DukeException("Deadline comes with a /at!");
+            throw new DukeException("Event comes with a /at!");
 
         else {
             String task = myString.substring(myString.indexOf(" "), index);
             String date = myString.substring(index + 3);
+            String temp = date;
+
             date = date.replaceAll(" ", "");
 
             if (date.isEmpty())
-                throw new DukeException("☹ OOPS!!! I'm sorry, but you need a time for your event");
+                throw new DukeException("☹ OOPS!!! I'm sorry, but you need a date/time for your event");
+            else if (temp.matches(" ([0-9]{1,2})/([0-9]{1,2})/([0-9]{4}) ([0-9]{4})")) {
+                SimpleDateFormat parser = new SimpleDateFormat("dd/MM/yyy HHmm");
+                SimpleDateFormat formatter = new SimpleDateFormat("dd MMM yyy, h:mma"); //Desired format
+                Date dateInput = parser.parse(temp);
+                date = formatter.format(dateInput);
+                System.out.println("Your specified deadline is " + date);
+            } else if (temp.matches(" ([0-9]{1,2})/([0-9]{1,2})/([0-9]{4})")) {
+                SimpleDateFormat parser = new SimpleDateFormat("dd/MM/yyy");
+                SimpleDateFormat formatter = new SimpleDateFormat("dd MMM yyy"); //Desired Format
+                Date dateInput = parser.parse(temp);
+                date = formatter.format(dateInput);
+                System.out.println("Your specified event is on the " + date);
+            } else
+                System.out.println("Do note that you did not specify any date/time for event though.");
 
-            Task newTask = new Event(task, date);
+            Task newTask = new Event(task, temp);
             System.out.println("______________________________");
             System.out.println("Got it. I've added this task:");
             System.out.println(newTask.toString());
